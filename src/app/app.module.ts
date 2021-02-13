6854c937accc85e7c6d211b0d372a1bdb2e3297e
kambac3 <kamil.baczynski96@gmail.com>
2021-02-13 23:51:00 +0100

    add component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { TodoComponent } from './todo/todo.component';
+import { TodoComponent } from './applications/todo/todo.component';
 
 @NgModule({
   declarations: [
